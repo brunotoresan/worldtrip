d78152c2b608305695701294244c29067476813b
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-28 15:49:59 -0300

    copied and pasted swiper recat demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Banner } from "./components/Banner";
 import { Header } from "./components/Header";
 import { TravelTypes } from "./components/TravelTypes";
+import ContinentCarousel from "./components/ContinentCarousel";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Banner />
       <TravelTypes />
+      <ContinentCarousel />
     </>
   )
 }

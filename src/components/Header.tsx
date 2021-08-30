e2ed60e89fbd546d13d1a4052a55a5fd5f629e2d
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-30 19:07:33 -0300

    added header return arrow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,28 @@
-import { Center, Image, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Image, Icon, Grid, Center } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+import { RiArrowLeftSLine } from 'react-icons/ri';
 
 export function Header(){
+  const { asPath } = useRouter()
+  const shouldHaveReturnArrow = asPath !== '/'
 
   return (
-    <Center>
-      <Image mt={[15, 27]} mb={[15, 27]} w={["81px","184px"]} src='/images/logo.svg' />
+    <Center as="header" mt={[15, 27]} mb={[15, 27]}>
+      <Grid
+        w="100%"
+        maxW='1240px'
+        mx="auto"
+        templateColumns="repeat(3, 1fr)"
+      >
+        { shouldHaveReturnArrow && 
+          <Link href='/' passHref>
+            <Icon as={RiArrowLeftSLine} fontSize={[20,40]} ml={["1rem", "0rem"]} cursor="pointer" justifySelf="start" />
+          </Link>
+        }
+        <Image w={["81px","184px"]} src='/images/logo.svg' justifySelf="center" />
+      </Grid>
     </Center>
   )
 }
+
--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,5 +1,7 @@
+import { Header } from "../../components/Header";
+
 export default function Continent(){
   return (
-    <h1>Europa</h1>
+    <Header />
   )
 }

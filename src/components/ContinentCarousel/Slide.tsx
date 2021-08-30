edcb79c7718e23e20d6a5095db080fe9a1377954
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-30 18:17:01 -0300

    adding Link to continent page

--- src/components/ContinentCarousel/Slide.tsx
+++ src/components/ContinentCarousel/Slide.tsx
@@ -1,4 +1,5 @@
 import { Text, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
 
 interface SlideProps {
   continentImage: string
@@ -8,21 +9,24 @@ interface SlideProps {
 
 export function Slide({continentImage, continentName, continentSlogan}: SlideProps){
   return (
-    <Flex
-      w='100%'
-      h='100%'
-      bgImage={`url(${continentImage})`}
-      align='center'
-      justify='center'
-      direction='column'
-      bgSize="cover"
-    >
-      <Heading color="gray.50" fontWeight="700" fontSize={["1.5rem", "3rem"]}>
-        {continentName}
-      </Heading>
-      <Text mt={["0.75rem", "1rem"]} fontSize={["0.875rem", "1.5rem"]} fontWeight="700" color="gray.200">
-        {continentSlogan}
-      </Text>
-    </Flex>
+    <Link href={`/continent/${continentName.toLowerCase()}`} passHref>
+      <Flex
+        w='100%'
+        h='100%'
+        bgImage={`url(${continentImage})`}
+        align='center'
+        justify='center'
+        direction='column'
+        bgSize="cover"
+        cursor="pointer"
+      >
+        <Heading color="gray.50" fontWeight="700" fontSize={["1.5rem", "3rem"]}>
+          {continentName}
+        </Heading>
+        <Text mt={["0.75rem", "1rem"]} fontSize={["0.875rem", "1.5rem"]} fontWeight="700" color="gray.200">
+          {continentSlogan}
+        </Text>
+      </Flex>
+    </Link>
   )
 }

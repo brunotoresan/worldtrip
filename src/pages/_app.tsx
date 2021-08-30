1aeaae3cbff7419e1667f6313dbf5e9f8010f871
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-30 17:59:25 -0300

    folder refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './styles/theme'
-import './styles/slider.scss';
+import { theme } from '../styles/theme'
+import '../styles/slider.scss';
 import "swiper/css";
 import "swiper/css/pagination"
 import "swiper/css/navigation"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { Banner } from "./components/Banner";
-import { Header } from "./components/Header";
-import { TravelTypes } from "./components/TravelTypes";
-import { ChooseYourContinent } from "./components/ChooseYourContinent";
-import ContinentCarousel from "./components/ContinentCarousel";
+import { Banner } from "../components/Banner";
+import { Header } from "../components/Header";
+import { TravelTypes } from "../components/TravelTypes";
+import { ChooseYourContinent } from "../components/ChooseYourContinent";
+import ContinentCarousel from "../components/ContinentCarousel";
 
 export default function Home() {
   return (

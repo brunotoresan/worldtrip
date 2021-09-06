ca59d3c0a22d4ae80eb8f54910bc749ebef1f674
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-09-05 23:03:03 -0300

    finishing code

--- src/components/ContinentCarousel/Slide.tsx
+++ src/components/ContinentCarousel/Slide.tsx
@@ -10,24 +10,26 @@ interface SlideProps {
 
 export function Slide({continentImage, continentSlug, continentName, continentSlogan}: SlideProps){
   return (
-    <Link href={`/continent/${continentSlug.toLowerCase()}`} passHref>
-      <Flex
-        w='100%'
-        h='100%'
-        bgImage={`url(${continentImage})`}
-        align='center'
-        justify='center'
-        direction='column'
-        bgSize="cover"
-        cursor="pointer"
-      >
-        <Heading color="gray.50" fontWeight="700" fontSize={["1.5rem", "3rem"]}>
-          {continentName}
-        </Heading>
-        <Text mt={["0.75rem", "1rem"]} fontSize={["0.875rem", "1.5rem"]} fontWeight="700" color="gray.200">
-          {continentSlogan}
-        </Text>
-      </Flex>
+    <Link href={`/continent/${continentSlug.toLowerCase()}`}>
+      <a>
+        <Flex
+          w='100%'
+          h='100%'
+          bgImage={`url(${continentImage})`}
+          align='center'
+          justify='center'
+          direction='column'
+          bgSize="cover"
+          cursor="pointer"
+        >
+          <Heading color="gray.50" fontWeight="700" fontSize={["1.5rem", "3rem"]}>
+            {continentName}
+          </Heading>
+          <Text mt={["0.75rem", "1rem"]} fontSize={["0.875rem", "1.5rem"]} fontWeight="700" color="gray.200">
+            {continentSlogan}
+          </Text>
+        </Flex>
+      </a>
     </Link>
   )
 }
